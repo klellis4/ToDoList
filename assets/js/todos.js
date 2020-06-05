402156c5e3056b921f10b276397102df7913d364
Kelly Ellis <49769233+klellis4@users.noreply.github.com>
2020-06-05 15:06:55 -0700

    Add initial files

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Add a new todo to the list
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		// Grab new Todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+
+		// Create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+// Toggle Add New Todo input box
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle();
+})
